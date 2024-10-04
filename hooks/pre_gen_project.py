71a7d07ba234557cd52de3cac40ac1397fe59a3e
Maher GZAM <110524435+mahergzam@users.noreply.github.com>
2024-10-04 11:40:48 +0200

    Update pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 def run_command(cmd):
@@ -5,6 +6,7 @@ def run_command(cmd):
 
 # Docker is always included, now decide which language template to use
 language_template = '{{ cookiecutter.language_template }}'
+project_slug = '{{ cookiecutter.project_slug }}'  # Get the project_slug from cookiecutter context
 
 # Map the language selection to repository URLs
 language_repos = {
@@ -16,18 +18,21 @@ language_repos = {
 # Clone the selected language template
 repo_url = language_repos.get(language_template)
 if repo_url:
-    # Clone the selected language template
-    run_command('git clone https://github.com/mahergzam/cookie-cutter-python temp_language')
+    print(f"Cloning {language_template} template from {repo_url}...")
+    run_command(f'git clone {repo_url} temp_language')
 
-    # Verify the clone was successful
+    # Verify that the repository was cloned correctly
     run_command('ls temp_language')
 
-    # Copy the contents from the project folder in the language template to the target folder
-    run_command('cp -r temp_language/{{ cookiecutter.project_slug }}/. {{ cookiecutter.project_slug }}/')
+    # Use the actual value of project_slug to copy the files
+    source_path = f'temp_language/{project_slug}/.'
+    target_path = f'{project_slug}/'
 
-    # Cleanup
+    # Copy files from the selected language template to the main project directory
+    run_command(f'cp -r {source_path} {target_path}')
+
+    # Clean up the temporary directory
     run_command('rm -rf temp_language')
-    print("Template files have been added to your project.")
-
+    print(f"{language_template.capitalize()} template files have been added to your project.")
 else:
     print(f"Error: Invalid language template '{language_template}' selected.")
