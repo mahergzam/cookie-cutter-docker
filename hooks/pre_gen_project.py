9b971d223347bd58d7ed1560a9c1ab112b37d6f4
Maher GZAM <110524435+mahergzam@users.noreply.github.com>
2024-10-04 11:33:09 +0200

    Update pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -16,16 +16,18 @@ language_repos = {
 # Clone the selected language template
 repo_url = language_repos.get(language_template)
 if repo_url:
-    print(f"Cloning {language_template} template from {repo_url}...")
-    run_command(f'git clone {repo_url} temp_language')
+    # Clone the selected language template
+    run_command('git clone https://github.com/user/python-template temp_language')
 
+    # Verify the clone was successful
     run_command('ls temp_language')
-    
-    # Copy files from the language template to the current project
-    run_command(f'cp -r temp_language/{{ cookiecutter.project_slug }}/. {{ cookiecutter.project_slug }}/')
-    
+
+    # Copy the contents from the project folder in the language template to the target folder
+    run_command('cp -r temp_language/{{ cookiecutter.project_slug }}/. {{ cookiecutter.project_slug }}/')
+
     # Cleanup
     run_command('rm -rf temp_language')
-    print(f"{language_template.capitalize()} template files have been added to your project.")
+    print("Template files have been added to your project.")
+
 else:
     print(f"Error: Invalid language template '{language_template}' selected.")
